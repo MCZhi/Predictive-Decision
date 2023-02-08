206c56238479b12205ceee33cdd53e2a4c0f8c34
Zhiyu Huang <34206160+MCZhi@users.noreply.github.com>
2023-02-08 17:25:35 +0800

    Update test.py

--- test.py
+++ test.py
@@ -41,7 +41,7 @@ def main(args):
 
     for scene in scenarios:
         print(f'============={scene}=============')
-        env = gym.make('smarts.env:driving_smarts_competition_v0_env', scenario=scene, headless=not args.envision_gui, sumo_headless=not args.sumo_gui)
+        env = gym.make('smarts.env.gymnasium:driving-smarts-competition-v0', scenario=scene, headless=not args.envision_gui, sumo_headless=not args.sumo_gui)
         observer = observation_adapter(env=env, num_neighbors=5)
 
         eval_episodes = args.episodes
