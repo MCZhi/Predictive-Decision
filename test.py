e9138a8c5b6065161663b0c93fe0d7b1e585cdd4
Zhiyu Huang <34206160+MCZhi@users.noreply.github.com>
2023-02-09 11:33:10 +0800

    Update test.py

--- test.py
+++ test.py
@@ -41,7 +41,7 @@ def main(args):
 
     for scene in scenarios:
         print(f'============={scene}=============')
-        env = gym.make('smarts.env.gymnasium:driving-smarts-competition-v0', action_space="TargetPose", scenario=scene, 
+        env = gym.make('smarts.env:multi-scenario-v0', scenario=scene, 
                        headless=not args.envision_gui, sumo_headless=not args.sumo_gui)
         observer = observation_adapter(env=env, num_neighbors=5)
 
